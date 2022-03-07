a616b5ef068644f7555d51589d3f1f5784e74682
Francisco Ruiz <franciscoruiz@Franciscos-MacBook-Pro.local>
2022-03-07 16:15:26 -0600

    Expresión Balanceada

---
+++ WeeklyChallenge2022.playground/Pages/Challenge10.xcplaygroundpage/Contents.swift
@@ -0,0 +1,46 @@
+//: [Previous](@previous)
+
+import Foundation
+
+/*
+ * Reto #10
+ * EXPRESIONES EQUILIBRADAS
+ * Fecha publicación enunciado: 07/03/22
+ * Fecha publicación resolución: 14/03/22
+ * Dificultad: MEDIA
+ *
+ * Enunciado: Crea un programa que comprueba si los paréntesis, llaves y corchetes de una expresión están equilibrados.
+ * - Equilibrado significa que estos delimitadores se abren y cieran en orden y de forma correcta.
+ * - Paréntesis, llaves y corchetes son igual de prioritarios. No hay uno más importante que otro.
+ * - Expresión balanceada: { [ a * ( c + d ) ] - 5 }
+ * - Expresión no balanceada: { a * ( c + d ) ] - 5 }
+ *
+ * Información adicional:
+ * - Usa el canal de nuestro discord (https://mouredev.com/discord) "🔁reto-semanal" para preguntas, dudas o prestar ayuda a la comunidad.
+ * - Puedes hacer un Fork del repo y una Pull Request al repo original para que veamos tu solución aportada.
+ * - Revisaré el ejercicio en directo desde Twitch el lunes siguiente al de su publicación.
+ * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
+ *
+ */
+
+func equilibrada(expresion: String) -> Bool {
+    var result: [Int] = []
+    
+    for char in expresion {
+        if let openIndex = ["[","{","("].firstIndex(of: String(char)) {
+            result.append(openIndex)
+        } else if let closeIndex = ["]","}",")"].firstIndex(of: String(char)) {
+            if let lastIndex = result.last, lastIndex == closeIndex {
+                result.popLast()
+            } else {
+                return false
+            }
+        }
+    }
+    
+    return result.isEmpty
+}
+
+print(equilibrada(expresion: "1+1"))
+
+//: [Next](@next)
